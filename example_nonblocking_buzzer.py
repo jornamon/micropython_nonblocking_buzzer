3397d9c0f661c54d365525bb0f297f3ec9975cbd
jornamon <jorge.nadal@gmail.com>
2022-07-28 12:23:24 -0600

    better stats in ms

--- example_nonblocking_buzzer.py
+++ example_nonblocking_buzzer.py
@@ -1,9 +1,10 @@
-# Nonblocking Buzzer for Micropython example
-# Plays some notes pattern while the main loop is running and calculates
-# some stats about the delay of the check() method, which is the one that needs
-# to be called from the main loop constantly
-# Jorge Nadal 2022
-
+"""
+Nonblocking Buzzer for Micropython example
+Plays some notes pattern while the main loop is running and calculates
+some stats about the delay that the check() method introduces in the main loop,
+which is the one that needs to be called from the main loop constantly
+jornamon 2022
+"""
 
 
 import time, math
@@ -14,20 +15,28 @@ from data_stats import data_stats
 
 
 nbb = NonBlocking_Buzzer(Pin(5))
-nbb.start((262*4,262*4,523*4), delay_note=100, delay_inter=50, delay_rep=300,reps=100)
+nbb.start((262*4,262*4,523*4), delay_note=50, delay_inter=25, delay_rep=100,reps=500)
 NUM_SAMPLES = 1000
-check_time_samples = array('I', [0] * NUM_SAMPLES)
+check_time_samples = array('f', [0] * NUM_SAMPLES)
 
 for i in range(NUM_SAMPLES):
     loop_start = time.ticks_us()
     nbb.check()
     time.sleep_ms(10)
-    check_time_samples[i] = time.ticks_diff(time.ticks_us(), loop_start)
-    print(check_time_samples[i])
+    check_time_samples[i] = time.ticks_diff(time.ticks_us(), loop_start) / 1000
+    #print(check_time_samples[i])
 
 nbb.stop()
 
-print(data_stats(check_time_samples))
 
+stats = data_stats(check_time_samples)
+print("Stats: (num samples, min, max, range, std. dev, avg, overhead)")
+print(stats['num_samples'])
+print(stats['min'])
+print(stats['max'])
+print(stats['range'])
+print(stats['stdev'])
+print(stats['avg'])
+print(stats['ovh'])
 
         
