2954a43f5fc1e929cf7986e4418227661f510183
jornamon <jorge.nadal@gmail.com>
2022-07-28 12:22:59 -0600

    better stats in ms

--- asyncio_buzzer.py
+++ asyncio_buzzer.py
@@ -5,7 +5,7 @@ This file is to test how this implementation affects de delay and timing of the 
 
 import uasyncio
 
-async def buzz(buzzer, notes, delay_note=100, delay_inter=50, delay_rep=300, reps=1, MUTE=False):
+async def buzz(buzzer, notes, delay_note=50, delay_inter=25, delay_rep=100, reps=1, MUTE=False):
     if not MUTE:
         for i in range(reps):
             for note in notes:
@@ -23,16 +23,24 @@ async def main():
     buzz(buzzer, (262*4,262*4,523*4), reps=3)
 
     NUM_SAMPLES = 1000
-    check_time_samples = array('I', [0] * NUM_SAMPLES)
+    check_time_samples = array('f', [0] * NUM_SAMPLES)
 
-    uasyncio.create_task(buzz(buzzer, (262*4,262*4,523*4), reps=100))
+    uasyncio.create_task(buzz(buzzer, (262*4,262*4,523*4), reps=500))
     for i in range(NUM_SAMPLES):
         loop_start = time.ticks_us()
         await uasyncio.sleep_ms(10)
-        check_time_samples[i] = time.ticks_diff(time.ticks_us(), loop_start)
+        check_time_samples[i] = time.ticks_diff(time.ticks_us(), loop_start) / 1000
         #print(check_time_samples[i])
         
-    print(data_stats(check_time_samples))
+    stats = data_stats(check_time_samples)
+    print("Stats: (num samples, min, max, range, std. dev, avg, overhead)")
+    print(stats['num_samples'])
+    print(stats['min'])
+    print(stats['max'])
+    print(stats['range'])
+    print(stats['stdev'])
+    print(stats['avg'])
+    print(stats['ovh'])
     buzzer.duty(0)
 
 
